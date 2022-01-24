a5f622484cffd45139ab3d39f0c5de800dbe1046
Colter Ulrich <cju5025@gmail.com>
2022-01-23 17:56:10 -0700

    Writes PanelBase Class

--- PanelBase.cpp
+++ PanelBase.cpp
@@ -1,1 +1,16 @@
 #include "PanelBase.h"
+
+PanelBase::PanelBase( XyteAudioProcessor* processor )
+:   mProcessor( processor )
+{
+    
+}
+
+void PanelBase::paint( Graphics& g )
+{
+    g.setColour( Colour( 67, 76, 80 ) );
+    g.fillAll();
+    
+    g.setColour( Colours::black );
+    g.drawRect( 0, 0, getWidth(), getHeight() );
+}
--- PanelBase.h
+++ PanelBase.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "JuceHeader.h"
+#include "PluginProcessor.h"
+#include "InterfaceDefines.h"
+
+class PanelBase
+:   public Component
+{
+public:
+  
+    PanelBase( XyteAudioProcessor* processor );
+    ~PanelBase();
+    
+    void paint ( Graphics& g ) override;
+    
+private:
+    
+    XyteAudioProcessor* mProcessor;
+    
+};
