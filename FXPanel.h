659826189f54a87018df55a7408b321750042b55
Colter Ulrich <cju5025@gmail.com>
2022-01-29 10:09:34 -0700

    Connects Mid & High Peak Dials

--- FXPanel.h
+++ FXPanel.h
@@ -19,4 +19,13 @@ private:
     std::unique_ptr<ParameterSlider> mLowBandFreqSlider;
     std::unique_ptr<ParameterSlider> mLowBandGainSlider;
     std::unique_ptr<ParameterSlider> mLowBandQualitySlider;
+    
+    std::unique_ptr<ParameterSlider> mMidBandFreqSlider;
+    std::unique_ptr<ParameterSlider> mMidBandGainSlider;
+    std::unique_ptr<ParameterSlider> mMidBandQualitySlider;
+    
+    std::unique_ptr<ParameterSlider> mHighBandFreqSlider;
+    std::unique_ptr<ParameterSlider> mHighBandGainSlider;
+    std::unique_ptr<ParameterSlider> mHighBandQualitySlider;
 };
+
